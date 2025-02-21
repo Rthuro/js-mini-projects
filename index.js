273a3f1c70909d2b34948be685d732cda2019a54
riyu.iring <oribelloruthiemy@gmail.com>
2025-02-21 23:22:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const projectContainer = document.getElementById('projectList');
 
 projects.forEach(project => {
     const a = document.createElement('a');
-    a.className = "p-2 rounded-lg border border-zinc-200 shadow w-[15rem] h-fit flex items-start gap-3 bg-white hover:shadow-lg";
+    a.className = "py-2 pl-3 pr-4 rounded-lg border border-zinc-200 shadow w-fit h-fit flex items-start gap-3 bg-white hover:shadow-lg";
     
     a.href = project.link;
 
     const iconContainer = document.createElement('div');
-    iconContainer.className = "bg-white rounded-lg border border-zinc-200 w-[2rem] p-2 flex items-center justify-center";
+    iconContainer.className = "bg-purple-50 rounded-lg border border-purple-300 w-[2rem] p-2 flex items-center justify-center";
     const icon = document.createElement('i');
     icon.setAttribute('data-lucide', project.icon);
+    icon.setAttribute('stroke', '#59168b');
     icon.className = "size-4";
+  
 
     iconContainer.appendChild(icon);
     a.appendChild(iconContainer);
