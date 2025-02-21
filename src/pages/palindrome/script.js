604e1ade7e987c07bcb7cf7bdc93bb6fdab615a3
riyu.iring <oribelloruthiemy@gmail.com>
2025-02-21 16:30:27 +0800

    Create script.js

---
+++ src/pages/palindrome/script.js
@@ -0,0 +1,40 @@
+
+
+const form = document.querySelector('form');
+const displayResult = document.getElementById('result');
+const checkText = document.getElementById('checkText');
+
+function cleanText(text){
+    const removeSpecialChar  = text.replace(/[_&\/\\#,|+()$~%.'":*?<>{}-]/g,"");
+    const toLowerCase = removeSpecialChar.toLowerCase();
+    return toLowerCase.split(' ').join('');
+}
+
+function checkIfPalindrome(text){
+    let reversedText = "";
+    const cleanedText = cleanText(text);
+    for(let i = text.length; i >= 0; i--){
+        reversedText += cleanedText.charAt(i)
+    }
+
+    if(reversedText == cleanedText){
+        return `Reversed: ${reversedText} <br> ${text} is a palindrome`;
+    }else{
+        return `Reversed: ${reversedText} <br> ${text} is not a palindrome`;
+    }
+  
+}
+
+checkText.addEventListener('input', ()=>{
+    if(checkText.value !== ''){
+        displayResult.innerHTML = '';
+        displayResult.classList.remove('hidden');
+        displayResult.style.color = "black";
+        displayResult.innerHTML = checkIfPalindrome(checkText.value);
+       
+    }else{
+        displayResult.classList.remove('hidden');
+        displayResult.style.color = "red";
+        displayResult.innerHTML = "Please enter a text";
+    }
+});
