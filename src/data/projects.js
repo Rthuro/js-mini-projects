9fa0ef810d0dc8fff1a1322c49b79c05ddb8eb57
riyu.iring <oribelloruthiemy@gmail.com>
2025-02-21 16:30:17 +0800

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,21 @@
+
+export const projects = [
+    {
+        id:1,
+        name: "To do list",
+        link: "/src/pages/to-do/index.html",
+        icon: "list-todo",
+    },{
+        id:2,
+        name: "Palindrome Checker",
+        link: "/src/pages/palindrome/index.html",
+        icon: "spell-check",
+    },{
+        id:3,
+        name: "Pokemon Search",
+        link: "/src/pages/pokemon/index.html",
+        icon: "bug",
+    },
+    
+
+];
